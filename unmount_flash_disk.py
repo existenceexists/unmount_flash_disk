30cfb3e5b40c28106c4d5f1c26a21b5f790be0ee
Kdokoli <you@example.com>
2019-05-06 09:45:37 +0200

    Enhance description displayed to user when the '--help' command line argument is given

--- unmount_flash_disk.py
+++ unmount_flash_disk.py
@@ -22,8 +22,8 @@ import sys
 def parse_arguments():
     d="""
 Unmount flash disk so it can be safely removed. 
-Prepare the application for all possible situations and errors.
-Developed for Xubuntu operating system. 
+Prepare the application for all possible situations and errors. 
+Developed for Xubuntu operating system in 2019-05, but it may work on other operating systems and versions too. 
 Software Python version 3.7 or higher is required to be installed, because the argument 'capture_output' of the method 'subprocess.run' was added in python 3.7 . 
 Software Zenity is required to be installed. 
 Software lsblk is required to be installed. 
